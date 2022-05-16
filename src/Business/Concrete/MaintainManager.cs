7e2b72b53fab796631ebd2c837a4369997f7c59d
beyzaerrdem <beyzardm502@gmail.com>
2022-05-16 18:10:54 +0300

    Added Business Code For Maintain and MaintainType

---
+++ src/Business/Concrete/MaintainManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class MaintainManager : IMaintainService
+    {
+        private readonly IMaintainDal _maintainDal;
+
+        public MaintainManager(IMaintainDal maintainDal)
+        {
+            _maintainDal = maintainDal;
+        }
+
+        public async Task AddMaintainAsync(Maintain maintain)
+        {
+            await _maintainDal.AddAsync(maintain);
+        }
+
+        public async Task<List<Maintain>> GetMaintainAsync()
+        {
+            var result = await _maintainDal.GetListAsync();
+            return result.ToList();
+        }
+
+        public Task<Maintain> GetMaintainByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateMaintainAsync(Maintain maintain)
+        {
+            await _maintainDal.UpdateAsync(maintain);
+        }
+    }
+}
