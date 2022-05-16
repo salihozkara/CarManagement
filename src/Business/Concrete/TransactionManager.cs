26b68e86413db250883766aac79abdc527d63a11
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-05-16 15:09:00 +0300

    Add transaction business code

---
+++ src/Business/Concrete/TransactionManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class TransactionManager : ITransactionService
+    {
+        private readonly ITransactionDal _transactionDal;
+
+        public TransactionManager(ITransactionDal transactionDal)
+        {
+            _transactionDal = transactionDal;
+        }
+
+        public async Task AddTransactionAsync(Transaction transaction)
+        {
+            await _transactionDal.AddAsync(transaction);
+        }
+
+        public async Task<List<Transaction>> GetTransactionAsync()
+        {
+            var result = await _transactionDal.GetListAsync();
+            return result.ToList();
+        }
+    }
+}
